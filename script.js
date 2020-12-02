4bfd1efd327a5920cdc58f5e0ebc47ae60e060d9
ujjwalsinghdev <ujjwalsinghmuj@gmail.com>
2020-12-03 00:34:36 +0530

    v1 commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let themeDots = document.getElementsByClassName("theme-dot")
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode
+    console.log("option clicked", mode)
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "main.css"
+  } else {
+    document.getElementById("theme-style").href = "dark.css"
+  }
+}
